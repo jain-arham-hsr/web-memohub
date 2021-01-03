987c65104d779dcd922104957a8cb7946ea288d0
jain_arham_hsr <expressor.bot@gmail.com>
2021-01-03 20:16:34 +0530

    Refactorisation

---
+++ helpers.py
@@ -0,0 +1,237 @@
+from flask import session, redirect, url_for
+from datetime import datetime
+import requests
+import json
+import re
+import ast
+from werkzeug.utils import import_string, cached_property
+from functools import wraps
+from decouple import config
+
+# noinspection PyProtectedMember
+from firebase_admin import credentials, initialize_app, storage, _apps, db, auth
+
+
+class LazyView(object):
+
+    def __init__(self, import_name):
+        self.__module__, self.__name__ = import_name.rsplit(".", 1)
+        self.import_name = import_name
+
+    @cached_property
+    def view(self):
+        return import_string(self.import_name)
+
+    def __call__(self, *args, **kwargs):
+        return self.view(*args, **kwargs)
+
+
+class Firebase:
+
+    default_profile_photo = "https://storage.googleapis.com/project-aa-e98db.appspot.com/defaultProfile"
+
+    def __init__(self):
+        if not _apps:
+            cred = credentials.Certificate("serviceAccountKey.json")
+            initialize_app(cred, {"databaseURL": "https://project-aa-e98db-default-rtdb.firebaseio.com",
+                                  "storageBucket": "project-aa-e98db.appspot.com"})
+        self._FIREBASE_WEB_API_KEY = config("FIREBASE_WEB_API_KEY")
+
+    def signup(self, f_name, l_name, email, password, user_cat):
+        endpoint = "https://identitytoolkit.googleapis.com/v1/accounts:signUp"
+        payload = json.dumps({
+            "email": email,
+            "password": password
+        })
+        response = requests.post(endpoint,
+                                 params={"key": self._FIREBASE_WEB_API_KEY},
+                                 data=payload).json()
+        try:
+            id_token = response['idToken']
+            local_id = response['localId']
+            self.update_profile(id_token, f"{f_name} {l_name}")
+            self.send_verification_email(id_token)
+            self.save_data(f"users/{local_id}", {
+                'category': user_cat,
+                'batches': [],
+                'theme': 'light'
+            })
+            check_for_pending_invitations(local_id, email, user_cat)
+        except:
+            raise Exception(response['error']['message'])
+
+    def login(self, email, password):
+        endpoint = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
+        payload = json.dumps({
+            "email": email,
+            "password": password,
+            "returnSecureToken": True
+        })
+        response = requests.post(endpoint,
+                                 params={"key": self._FIREBASE_WEB_API_KEY},
+                                 data=payload).json()
+        try:
+            id_token = response['idToken']
+            user_data = self.get_user_data(id_token)['users'][0]
+            email_verified = user_data["emailVerified"]
+            if email_verified:
+                return user_data['localId'], user_data['displayName']
+            else:
+                self.send_verification_email(id_token)
+        except:
+            raise Exception(response['error']['message'])
+
+    def get_user_data(self, id_token):
+        endpoint = "https://identitytoolkit.googleapis.com/v1/accounts:lookup"
+        payload = json.dumps({
+            "idToken": id_token,
+        })
+        response = requests.post(endpoint,
+                                 params={"key": self._FIREBASE_WEB_API_KEY},
+                                 data=payload).json()
+        return response
+
+    @staticmethod
+    def get_user_by_uid(uid):
+        return auth.get_user(uid)
+
+    @staticmethod
+    def get_user_by_email(email):
+        return auth.get_user_by_email(email)
+
+    def update_profile(self, id_token, display_name, photo_url=default_profile_photo):
+        endpoint = "https://identitytoolkit.googleapis.com/v1/accounts:update"
+        payload = json.dumps({
+            "idToken": id_token,
+            "displayName": display_name,
+            "photoUrl": photo_url,
+            "deleteAttribute": [],
+            "returnSecureToken": True
+        })
+        requests.post(endpoint,
+                      params={"key": self._FIREBASE_WEB_API_KEY},
+                      data=payload)
+
+    def send_password_reset_email(self, email):
+        endpoint = 'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode'
+        payload = json.dumps({
+            "requestType": "PASSWORD_RESET",
+            "email": email
+        })
+        response = requests.post(endpoint,
+                                 params={"key": self._FIREBASE_WEB_API_KEY},
+                                 data=payload).json()
+        if 'error' in response.keys():
+            raise Exception(response['error']['message'])
+
+    def send_verification_email(self, id_token):
+        endpoint = "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode"
+        payload = json.dumps({
+            "requestType": "VERIFY_EMAIL",
+            "idToken": id_token
+        })
+        requests.post(endpoint,
+                      params={"key": self._FIREBASE_WEB_API_KEY},
+                      data=payload)
+
+    @staticmethod
+    def upload_file_to_storage(binary_file, filename, content_type="application/octet-stream"):
+        bucket = storage.bucket()
+        blob = bucket.blob(filename)
+        blob.upload_from_file(binary_file, content_type=content_type)
+        blob.make_public()
+        return blob.public_url
+
+    @staticmethod
+    def save_data(key, value):
+        ref = db.reference(key)
+        ref.set(value)
+
+    @staticmethod
+    def retrieve_data(key):
+        ref = db.reference(key)
+        return ref.get()
+
+    @staticmethod
+    def append_data(key, value_to_append):
+        batches_array = Firebase.retrieve_data(key)
+        if batches_array:
+            batches_array.append(value_to_append)
+            Firebase.save_data(key, batches_array)
+        else:
+            Firebase.save_data(key, [value_to_append])
+
+    @staticmethod
+    def remove_list_item(key, value_to_remove):
+        batches_array = Firebase.retrieve_data(key)
+        batches_array.remove(value_to_remove)
+        Firebase.save_data(key, batches_array)
+
+
+class Memohub:
+
+    @staticmethod
+    def send_text_msg(batch_id, sender, msg):
+        Firebase.append_data(f'batches/batch_{batch_id}/messages', {
+            'timestamp': datetime.now().strftime("%H:%M %B %d, %Y"),
+            'type': 'text',
+            'sender': sender,
+            'value': msg
+        })
+
+    @staticmethod
+    def send_attach_msg(batch_id, sender, topic, file_url):
+        Firebase.append_data(f'batches/batch_{batch_id}/messages', {
+            'timestamp': datetime.now().strftime("%H:%M %B %d, %Y"),
+            'type': 'file',
+            'sender': sender,
+            'value': [topic, file_url]
+        })
+
+
+# login required decorator function
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'uid' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('auth', action="login"))
+    return wrap
+
+
+def set_theme():
+    session['profile_data']['theme'] = ast.literal_eval(config("THEME"))[Firebase.retrieve_data(f"users/{session.get('uid')}/theme")]
+
+
+def check_for_pending_invitations(uid, email, user_cat):
+    formatted_email = format_email(email)
+    if formatted_email in Firebase.retrieve_data('pendingInvitation').keys():
+        pending_batch_invitations = Firebase.retrieve_data(f'pendingInvitation/{formatted_email}')
+        # noinspection PyTypeChecker
+        for invitation in pending_batch_invitations:
+            Firebase.append_data(f"users/{uid}/batches", invitation)
+            batch_participants = Firebase.retrieve_data(f'batches/{invitation}/participants')
+            batch_participants[batch_participants.index([email, 'undefined'])] = (email, user_cat)
+            Firebase.save_data(f'batches/{invitation}/participants', batch_participants)
+        Firebase.save_data('pendingInvitation', Firebase.retrieve_data('pendingInvitation').pop(formatted_email, None))
+
+
+def validate_duplicate_batches(name, section, subject):
+    batches = Firebase.retrieve_data(f'users/{session["uid"]}/batches') or []
+    for batch in batches:
+        corresponding_batch_data = Firebase.retrieve_data(f'batches/{batch}')
+        if all([corresponding_batch_data['name'] == name,
+                corresponding_batch_data['section'] == section,
+                corresponding_batch_data['subject'] == subject]):
+            return True
+    return False
+
+
+def validate_file_format(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ast.literal_eval(config("ALLOWED_EXTENSIONS")).keys()
+
+
+def format_email(email):
+    return re.sub("[^a-zA-Z]", "_", email)
---
+++ views.py
@@ -0,0 +1,263 @@
+from flask import session, request, render_template, redirect, url_for, flash
+# noinspection PyProtectedMember
+from firebase_admin._auth_utils import UserNotFoundError
+from werkzeug.utils import secure_filename
+from datetime import datetime
+from decouple import config
+import ast
+
+from memohub.helpers import login_required, Firebase, Memohub, set_theme, validate_duplicate_batches, validate_file_format, format_email
+
+
+Firebase = Firebase()
+
+
+def home():
+    if session.get('uid', None):
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('home.html', signed_out=True)
+
+
+def auth(action):
+    locale_error_msg = session.get('error_msg', None)
+    session.pop('error_msg', None)
+    return render_template('auth.html', action=action, error_msg=locale_error_msg, signed_out=True)
+
+
+def auth_verification():
+    if request.method == 'POST':
+        if request.form['action'] == "Log In":
+            email = request.form['email']
+            password = request.form['password']
+            # noinspection PyBroadException
+            try:
+                uid, display_name = Firebase.login(email, password)
+            except Exception:
+                session['error_msg'] = Exception
+                return redirect(url_for('auth', action="login"))
+            session['uid'] = uid
+            session['display_name'] = display_name
+            session['email'] = email
+            session['user_cat'] = Firebase.retrieve_data(f"users/{session.get('uid')}/category")
+            session['profile_data'] = {
+                "displayName": display_name,
+                "email": email,
+                "userCat": session.get('user_cat')
+            }
+            return redirect(url_for('dashboard'))
+        else:
+            f_name = request.form['f_name']
+            l_name = request.form['l_name']
+            email = request.form['email']
+            password = request.form['password']
+            user_category = "teacher" if request.form['action'] == 'Sign Up as Teacher' else "student"
+            # noinspection PyBroadException
+            try:
+                Firebase.signup(f_name, l_name, email, password, user_category)
+                return redirect(url_for('auth', action="login"))
+            except Exception:
+                session['error_msg'] = Exception
+                return redirect(url_for('auth', action="signup"))
+    set_theme()
+    return render_template('404.html')
+
+
+def reset_password():
+    if request.method == 'POST':
+        # noinspection PyBroadException
+        try:
+            Firebase.send_password_reset_email(request.form['forgot-pass-email'])
+        except Exception:
+            session['error_msg'] = Exception
+        return redirect(url_for('logout'))
+    set_theme()
+    return render_template('404.html')
+
+
+@login_required
+def dashboard():
+    dashboard_template_name = "teacher_dashboard.html" if session.get("user_cat") == "teacher" else "dashboard.html"
+    batches = Firebase.retrieve_data(f"users/{session.get('uid')}/batches") or []
+    for index, batch_name in enumerate(batches):
+        batch_data = Firebase.retrieve_data(f"batches/{batch_name}")
+        batch_data['creatorName'] = Firebase.get_user_by_uid(batch_data['created-by']).display_name
+        batches[index] = batch_data
+    decks = list(zip(*[iter(batches)]*3))
+    if len(batches) % 3 != 0:
+        decks.append(tuple(batches[(-1) * (len(batches) % 3):]))
+    if not all(decks):
+        decks = None
+    set_theme()
+    return render_template(dashboard_template_name, decks=decks, profile_data=session.get('profile_data'))
+
+
+def create_batch():
+    if request.method == 'POST':
+        if validate_duplicate_batches(request.form['class-name'],
+                                      request.form['section'],
+                                      request.form['subject']):
+            flash("Couldn't create batch due to duplicate data.")
+            return redirect(url_for('dashboard'))
+        batch_id = datetime.now().strftime("%Y%m%d%H%M%S%f%z")
+        Firebase.save_data(f'batches/batch_{batch_id}', {
+            'name': request.form['class-name'],
+            'section': request.form['section'],
+            'subject': request.form['subject'],
+            'batch_id': batch_id,
+            'created-by': session.get('uid'),
+            'creation-date': datetime.now().strftime("%B %d, %Y"),
+            'messages': [
+                {
+                    'timestamp': datetime.now().strftime("%H:%M %B %d, %Y"),
+                    'type': 'text',
+                    'sender': 'MemoHub',
+                    'value': f'{request.form["class-name"]} "{request.form["section"]}" ({request.form["subject"]}) created.'
+                }
+            ],
+            'participants': [(session.get('email'), session.get('user_cat'))],
+            'active': True
+        })
+        Firebase.append_data(f'users/{session["uid"]}/batches', f'batch_{batch_id}')
+        return redirect(url_for('dashboard'))
+    set_theme()
+    return render_template('404.html')
+
+
+@login_required
+def batch(batch_id):
+    if f"batch_{batch_id}" in Firebase.retrieve_data(f'users/{session["uid"]}/batches'):
+        session['last_batch_opened'] = batch_id
+        batch_data = Firebase.retrieve_data(f'batches/batch_{batch_id}')
+        locale_error_msg = session.get('error_msg')
+        session.pop('error_msg', None)
+        for idx, msg in enumerate(batch_data['messages']):
+            if msg['type'] == 'text':
+                batch_data['messages'][idx]['value'] = msg['value'].replace("\r\n", "<br>")
+        batch_data['participants'].remove([session.get('email'), session.get('user_cat')])
+        is_creator = session.get('uid', None) == Firebase.retrieve_data(f'batches/batch_{batch_id}/created-by')
+        set_theme()
+        return render_template('memos.html', cat=session.get('user_cat'), batch_data=batch_data, error_msg=locale_error_msg, is_creator=is_creator, profile_data=session.get('profile_data'))
+    set_theme()
+    return render_template('404.html')
+
+
+def add_participant():
+    if request.method == 'POST':
+        batch_id = session.get('last_batch_opened')
+        email = request.form['email']
+        try:
+            user_data = Firebase.get_user_by_email(email)
+            if f"batch_{batch_id}" not in (Firebase.retrieve_data(f'users/{user_data.uid}/batches') or []):
+                cat = Firebase.retrieve_data(f'users/{user_data.uid}/category')
+                Firebase.append_data(f'users/{user_data.uid}/batches', f"batch_{batch_id}")
+                Firebase.append_data(f'batches/batch_{batch_id}/participants', (request.form['email'], cat))
+                Memohub.send_text_msg(batch_id, "MemoHub", f"'{user_data.display_name}' added to this batch.")
+            else:
+                session['error_msg'] = "User already enrolled in this batch. Please enter email of some other user."
+        except UserNotFoundError:
+            if f"batch_{batch_id}" not in (Firebase.retrieve_data(f"pendingInvitation/{format_email(email)}") or []):
+                Firebase.append_data(f"pendingInvitation/{format_email(email)}", f"batch_{batch_id}")
+                Firebase.append_data(f'batches/batch_{batch_id}/participants', (request.form['email'], 'undefined'))
+                Memohub.send_text_msg(batch_id, "MemoHub", f"'{email}' added to this batch.")
+            else:
+                session['error_msg'] = "User already enrolled in this batch. Please enter email of some other user."
+        except ValueError:
+            session['error_msg'] = "Please enter a valid email address."
+        return redirect(url_for('batch', batch_id=batch_id))
+    set_theme()
+    return render_template('404.html')
+
+
+def remove_participant():
+    if request.method == 'POST':
+        email, cat = request.form['participant'].split(',')
+        batch_id = session.get('last_batch_opened', None)
+        try:
+            user_data = Firebase.get_user_by_email(email)
+            Firebase.remove_list_item(f'users/{user_data.uid}/batches', f'batch_{batch_id}')
+            Memohub.send_text_msg(batch_id, "MemoHub", f"'{user_data.display_name}' removed from this batch.")
+        except UserNotFoundError:
+            Firebase.remove_list_item(f'pendingInvitation/{format_email(email)}', f'batch_{batch_id}')
+            Memohub.send_text_msg(batch_id, "MemoHub", f"'{email}' removed from this batch.")
+        Firebase.remove_list_item(f'batches/batch_{batch_id}/participants', [email, cat])
+        return redirect(url_for('batch', batch_id=batch_id))
+    set_theme()
+    return render_template('404.html')
+
+
+def delete_batch():
+    if request.method == 'POST':
+        batch_id = session.get('last_batch_opened', None)
+        Firebase.save_data(f'batches/batch_{batch_id}/active', False)
+        return redirect(url_for('batch', batch_id=batch_id))
+    set_theme()
+    return render_template('404.html')
+
+
+def remove_batch():
+    if request.method == 'POST':
+        batch_id = session.get('last_batch_opened', None)
+        uid = session.get('uid', None)
+        Firebase.remove_list_item(f'users/{uid}/batches', f'batch_{batch_id}')
+        return redirect(url_for('home'))
+    set_theme()
+    return render_template('404.html')
+
+
+def send_text_msg():
+    if request.method == 'POST':
+        batch_id = session.get('last_batch_opened')
+        Memohub.send_text_msg(batch_id, session.get('display_name'), request.form['msg'])
+        return redirect(url_for('batch', batch_id=batch_id))
+    set_theme()
+    return render_template('404.html')
+
+
+def send_attach_msg():
+    if request.method == 'POST':
+        batch_id = session.get('last_batch_opened')
+        files = request.files.getlist("files")
+        for file in files:
+            if validate_file_format(file.filename):
+                sender = session.get('display_name')
+                file_id = datetime.now().strftime("%Y%m%d%H%M%S%f%z")
+                file_url = Firebase.upload_file_to_storage(file,
+                                                           f"file_{file_id}.{secure_filename(file.filename).rsplit('.', 1)[1].lower()}",
+                                                           ast.literal_eval(config("ALLOWED_EXTENSIONS"))[secure_filename(file.filename).rsplit('.', 1)[1].lower()])
+                topic = file.filename
+                Memohub.send_attach_msg(batch_id, sender, topic, file_url)
+            else:
+                flash(f"Could not upload {file.filename} due to invalid file type")
+        return redirect(url_for('batch', batch_id=batch_id))
+    set_theme()
+    return render_template('404.html')
+
+
+def not_found_error(_):
+    if session.get('uid', None):
+        set_theme()
+    signed_out = False if session.get('uid', None) else True
+    return render_template('404.html', profile_data=session.get('profile_data'), signed_out=signed_out), 404
+
+
+def method_not_allowed_error(_):
+    if session.get('uid', None):
+        set_theme()
+    signed_out = False if session.get('uid', None) else True
+    return render_template('404.html', profile_data=session.get('profile_data'), signed_out=signed_out), 405
+
+
+def change_theme():
+    if request.method == 'POST':
+        uid = session.get('uid', None)
+        Firebase.save_data(f"users/{uid}/theme", request.form['theme'])
+        set_theme()
+        return redirect(url_for('dashboard'))
+    set_theme()
+    return render_template('404.html')
+
+
+def logout():
+    session.pop('uid', None)
+    return redirect(url_for('home'))
